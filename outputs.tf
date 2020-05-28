2c6f557d0d3c3bcd96e66e233de81d0053738b33
Alex Vincent <alex.vincent@geoplex.com.au>
2020-05-28 17:15:59 +1000

    Minimal changes to make ALB optional and the web acl exportable

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,13 @@
+locals {
+  waf_outputs = coalescelist(aws_cloudformation_stack.waf.*.outputs, [{}])[0]
+}
 
+output waf_name {
+  description = "The name of the created WAF"
+  value       = lookup(local.waf_outputs, "WAFWebName", null)
+}
+
+output waf_id {
+  description = "The id of the created WAF"
+  value       = lookup(local.waf_outputs, "WAFWebId", null)
+}

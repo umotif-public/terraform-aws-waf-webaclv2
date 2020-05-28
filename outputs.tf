038b215780f9e2ab2213e02caa328367f69fdc99
Alex Vincent <2034948+ASVincent@users.noreply.github.com>
2020-05-28 19:53:57 +1000

    Allow conditional association with ALB
    * Minimal changes to make ALB optional and the web acl exportable
    
    * Switch id to ARN due to confusing wafregional and wafv2
    
    Co-authored-by: Alex Vincent <alex.vincent@geoplex.com.au>

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,13 @@
+locals {
+  waf_outputs = coalescelist(aws_cloudformation_stack.waf.*.outputs, [{}])[0]
+}
 
+output waf_name {
+  description = "The name of the created WAF Web ACL"
+  value       = lookup(local.waf_outputs, "WAFWebName", null)
+}
+
+output waf_arn {
+  description = "The arn of the created WAF Web ACL"
+  value       = lookup(local.waf_outputs, "WAFWebArn", null)
+}

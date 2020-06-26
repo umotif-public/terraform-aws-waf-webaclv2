35c96820e85cd8979ca1d6cfd71bb78b8e9267ff
Marcin Cuber <marcincuber@hotmail.com>
2020-06-26 15:02:14 +0100

    Update example and fix example config (#9)

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_wafv2_web_acl_association" "main" {
 resource "aws_wafv2_web_acl_logging_configuration" "main" {
   count = var.enabled && var.create_logging_configuration ? 1 : 0
 
-  log_destination_configs = var.log_destination_configs #["${aws_kinesis_firehose_delivery_stream.example.arn}"]
+  log_destination_configs = var.log_destination_configs
   resource_arn            = aws_wafv2_web_acl.main[0].arn
 
   dynamic "redacted_fields" {

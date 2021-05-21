80231c5db0f2b91fb636dca689be4e0ef3107c9e
Sean Pascual <sean.pascual@umotif.com>
2021-05-21 12:15:35 +0100

    Added bytematch rules

---
+++ examples/wafv2-bytematch-rules/main.tf
@@ -0,0 +1,94 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+#####
+# Web Application Firewall configuration
+#####
+module "waf" {
+  source = "../.."
+
+  name_prefix = "test-waf-setup"
+  alb_arn     = aws_lb.test.arn
+
+  allow_default_action = true
+
+  scope = "REGIONAL"
+
+  create_alb_association = true
+
+  visibility_config = {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "test-waf-setup-waf-main-metrics"
+    sampled_requests_enabled   = false
+  }
+
+  rules = [
+    {
+      name     = "AWSManagedRulesCommonRuleSet-rule-1"
+      priority = "1"
+
+      override_action = "none"
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        metric_name                = "AWSManagedRulesCommonRuleSet-metric"
+        sampled_requests_enabled   = false
+      }
+
+      managed_rule_group_statement = {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+        excluded_rule = [
+          "SizeRestrictions_QUERYSTRING",
+          "SizeRestrictions_BODY",
+          "GenericRFI_QUERYARGUMENTS"
+        ]
+      }
+    },
+    {
+      name     = "block-specific-uri-path"
+      priority = 2
+      action   = "block"
+
+      byte_match_statement = {
+        field_to_match = {
+          uri_path = "{}"
+        }
+        positional_constraint = "STARTS_WITH"
+        search_string         = "/path/to/match"
+        priority              = 0
+        type                  = "NONE"
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+      }
+    },
+    {
+      name     = "block-if-request-body-contains-hotmail-email"
+      priority = 2
+      action   = "block"
+
+      byte_match_statement = {
+        field_to_match = {
+          body = "{}"
+        }
+        positional_constraint = "CONTAINS"
+        search_string         = "@hotmail.com"
+        priority              = 0
+        type                  = "NONE"
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+      }
+    }
+  ]
+
+  tags = {
+    "Environment" = "test"
+  }
+}
---
+++ examples/wafv2-geo-rules/main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+#####
+# Web Application Firewall configuration
+#####
+module "waf" {
+  source = "../.."
+
+  name_prefix = "test-waf-setup"
+  alb_arn     = aws_lb.test.arn
+
+  allow_default_action = true
+
+  scope = "REGIONAL"
+
+  create_alb_association = true
+
+  visibility_config = {
+    cloudwatch_metrics_enabled = false
+    metric_name                = "test-waf-setup-waf-main-metrics"
+    sampled_requests_enabled   = false
+  }
+
+  rules = [
+    {
+      name     = "AWSManagedRulesCommonRuleSet-rule-1"
+      priority = "1"
+
+      override_action = "none"
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        metric_name                = "AWSManagedRulesCommonRuleSet-metric"
+        sampled_requests_enabled   = false
+      }
+
+      managed_rule_group_statement = {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+        excluded_rule = [
+          "SizeRestrictions_QUERYSTRING",
+          "SizeRestrictions_BODY",
+          "GenericRFI_QUERYARGUMENTS"
+        ]
+      }
+    },
+    {
+      name     = "allow-nl-gb-us-traffic-only"
+      priority = 2
+      action   = "allow"
+
+      geo_match_statement = {
+        country_codes = ["NL", "GB", "US"]
+      }
+
+      visibility_config = {
+        cloudwatch_metrics_enabled = false
+        sampled_requests_enabled   = false
+      }
+    }
+  ]
+
+  tags = {
+    "Environment" = "test"
+  }
+}

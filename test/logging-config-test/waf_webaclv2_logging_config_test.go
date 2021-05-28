7ac0c539fcc6963b09fc8d42dd5f22e68aeef01b
Sean Pascual <sean.pascual@umotif.com>
2021-05-28 11:22:29 +0100

    DEVOPS-957 Terratests

---
+++ test/logging-config-test/waf_webaclv2_logging_config_test.go
@@ -0,0 +1,78 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWafWebAclV2Core(t *testing.T) {
+	// retryable errors in terraform testing.
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../../examples/wafv2-logging-configuration",
+		Upgrade:      true,
+	})
+
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables
+	// WebACL outputs
+	WebAclName := terraform.Output(t, terraformOptions, "web_acl_name")
+	WebAclArn := terraform.Output(t, terraformOptions, "web_acl_arn")
+	WebAclCapacity := terraform.Output(t, terraformOptions, "web_acl_capacity")
+	WebAclVisConfigMetricName := terraform.Output(t, terraformOptions, "web_acl_visibility_config_name")
+
+	// Rule outputs
+	WebAclRuleNames := terraform.Output(t, terraformOptions, "web_acl_rule_names")
+
+	// Web ACL Association Outputs
+	WebAclAssociationId := terraform.Output(t, terraformOptions, "web_acl_assoc_id")
+	WebAclAssociationResourceArn := terraform.Output(t, terraformOptions, "web_acl_assoc_resource_arn")
+	WebAclAssociationAclArn := terraform.Output(t, terraformOptions, "web_acl_assoc_acl_arn")
+
+	// Logging Outputs
+	KinesisStreamArn := terraform.Output(t, terraformOptions, "kinesis_firehose_delivery_stream_arn")
+	IamRoleArn := terraform.Output(t, terraformOptions, "logging_iam_role_arn")
+	IamRoleId := terraform.Output(t, terraformOptions, "logging_iam_role_id")
+	IamRoleName := terraform.Output(t, terraformOptions, "logging_iam_role_name")
+	IamRolePolicyId := terraform.Output(t, terraformOptions, "logging_iam_role_policy_id")
+	IamRolePolicyName := terraform.Output(t, terraformOptions, "logging_iam_role_policy_name")
+	IamRolePolicyRole := terraform.Output(t, terraformOptions, "logging_iam_role_policy_role")
+	S3BucketArn := terraform.Output(t, terraformOptions, "logging_s3_bucket_arn")
+	S3BucketId := terraform.Output(t, terraformOptions, "logging_s3_bucket_id")
+
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, WebAclName, "test-waf-setup")
+	assert.Contains(t, WebAclArn, "arn:aws:wafv2:eu-west-1:")
+	assert.Contains(t, WebAclArn, "regional/webacl/test-waf-setup")
+	assert.Equal(t, WebAclVisConfigMetricName, "test-waf-setup-waf-main-metrics")
+	assert.Equal(t, WebAclCapacity, "950")
+	assert.Equal(t, WebAclRuleNames, "AWSManagedRulesCommonRuleSet-rule-1, AWSManagedRulesKnownBadInputsRuleSet-rule-2, AWSManagedRulesPHPRuleSet-rule-3")
+
+	assert.Contains(t, WebAclAssociationId, "arn:aws:wafv2:eu-west-1")
+	assert.Contains(t, WebAclAssociationId, "regional/webacl/test-waf-setup")
+	assert.Contains(t, WebAclAssociationResourceArn, "arn:aws:elasticloadbalancing:eu-west-1")
+	assert.Contains(t, WebAclAssociationResourceArn, "loadbalancer/app/alb-waf-example")
+	assert.Contains(t, WebAclAssociationAclArn, "arn:aws:wafv2:eu-west-1:")
+	assert.Contains(t, WebAclAssociationAclArn, "regional/webacl/test-waf-setup")
+
+	assert.Contains(t, KinesisStreamArn, "arn:aws:firehose:eu-west-1")
+	assert.Contains(t, KinesisStreamArn, "deliverystream/aws-waf-logs-kinesis-firehose-test-stream")
+
+	assert.Contains(t, IamRoleArn, "arn:aws:iam::")
+	assert.Contains(t, IamRoleArn, "role/firehose-stream-test-role")
+	assert.Equal(t, IamRoleId, "firehose-stream-test-role")
+	assert.Equal(t, IamRoleName, "firehose-stream-test-role")
+
+	assert.Equal(t, IamRolePolicyId, "firehose-stream-test-role:firehose-role-custom-policy")
+	assert.Equal(t, IamRolePolicyName, "firehose-role-custom-policy")
+	assert.Equal(t, IamRolePolicyRole, "firehose-stream-test-role")
+
+	assert.Equal(t, S3BucketArn, "arn:aws:s3:::firehose-stream-test-bucket")
+	assert.Equal(t, S3BucketId, "firehose-stream-test-bucket")
+}

b5672c1249858300af4292e08d7c3e4ca117e8e8
Felipe Zipitria <81985867+fzipi360@users.noreply.github.com>
2021-10-20 09:46:10 -0300

    feat(rules): add size constraint statement (#41)
    * feat(rules): add size constraint statement
    
    Signed-off-by: Felipe Zipitria <fzipitria@life360.com>
    
    * feat(terratest): add tests to sizeconstraint statement
    
    Signed-off-by: Felipe Zipitria <fzipitria@life360.com>
    
    * fix(ci): update go modules to fix terratest in GHA
    
    Signed-off-by: Felipe Zipitria <fzipitria@life360.com>

--- main.tf
+++ main.tf
@@ -400,6 +400,49 @@ resource "aws_wafv2_web_acl" "main" {
           }
         }
 
+        dynamic "size_constraint_statement" {
+          for_each = length(lookup(rule.value, "size_constraint_statement", {})) == 0 ? [] : [lookup(rule.value, "size_constraint_statement", {})]
+          content {
+            dynamic "field_to_match" {
+              for_each = length(lookup(size_constraint_statement.value, "field_to_match", {})) == 0 ? [] : [lookup(size_constraint_statement.value, "field_to_match", {})]
+              content {
+                dynamic "uri_path" {
+                  for_each = length(lookup(field_to_match.value, "uri_path", {})) == 0 ? [] : [lookup(field_to_match.value, "uri_path")]
+                  content {}
+                }
+                dynamic "all_query_arguments" {
+                  for_each = length(lookup(field_to_match.value, "all_query_arguments", {})) == 0 ? [] : [lookup(field_to_match.value, "all_query_arguments")]
+                  content {}
+                }
+                dynamic "body" {
+                  for_each = length(lookup(field_to_match.value, "body", {})) == 0 ? [] : [lookup(field_to_match.value, "body")]
+                  content {}
+                }
+                dynamic "method" {
+                  for_each = length(lookup(field_to_match.value, "method", {})) == 0 ? [] : [lookup(field_to_match.value, "method")]
+                  content {}
+                }
+                dynamic "query_string" {
+                  for_each = length(lookup(field_to_match.value, "query_string", {})) == 0 ? [] : [lookup(field_to_match.value, "query_string")]
+                  content {}
+                }
+                dynamic "single_header" {
+                  for_each = length(lookup(field_to_match.value, "single_header", {})) == 0 ? [] : [lookup(field_to_match.value, "single_header")]
+                  content {
+                    name = lower(lookup(single_header.value, "name"))
+                  }
+                }
+              }
+            }
+            comparison_operator = lookup(size_constraint_statement.value, "comparison_operator")
+            size                = lookup(size_constraint_statement.value, "size")
+            text_transformation {
+              priority = lookup(size_constraint_statement.value, "priority")
+              type     = lookup(size_constraint_statement.value, "type")
+            }
+          }
+        }
+
         dynamic "rate_based_statement" {
           for_each = length(lookup(rule.value, "rate_based_statement", {})) == 0 ? [] : [lookup(rule.value, "rate_based_statement", {})]
           content {

7a3e8b3c45fda4f9601b39a089ac3f5b97ab56d0
ndugera <ndugera@payfone.com>
2022-12-22 11:25:49 +0000

    Updating sizeconstraint rule to have oversizehandling for body.

--- main.tf
+++ main.tf
@@ -1094,7 +1094,9 @@ resource "aws_wafv2_web_acl" "main" {
                 }
                 dynamic "body" {
                   for_each = length(lookup(field_to_match.value, "body", {})) == 0 ? [] : [lookup(field_to_match.value, "body")]
-                  content {}
+                  content {
+                    OversizeHandling : upper(lookup(body.value, "OversizeHandling"))
+                  }
                 }
                 dynamic "method" {
                   for_each = length(lookup(field_to_match.value, "method", {})) == 0 ? [] : [lookup(field_to_match.value, "method")]
